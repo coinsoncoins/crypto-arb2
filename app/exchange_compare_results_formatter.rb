f63c7f2d2a9f9b9748eb2346a95f290bf638d06e
Coins On Coins <coinsoncoins00@gmail.com>
2017-10-21 10:56:34 -0500

    tests for exchange_compare_results_formatter

--- app/exchange_compare_results_formatter.rb
+++ app/exchange_compare_results_formatter.rb
@@ -2,7 +2,7 @@ class ExchangeCompareResultsFormatter
 
   def self.format(results)
     "Buy #{results.market} at #{results.exchange1} at ask #{results.ask1} (volume #{results.volume1}) " \
-    "and sell at #{results.exchange2} at bid #{results.bid2} (volume #{results.volume2}) for gain #{results.gain.round*100.0}%"
+    "and sell at #{results.exchange2} at bid #{results.bid2} (volume #{results.volume2}) for gain #{(results.gain*100.0).round}%"
   end
 
 end
