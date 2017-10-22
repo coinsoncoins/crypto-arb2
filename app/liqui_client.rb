8c11489625d2b4129eed8c9623a8207e550e9c28
Coins On Coins <coinsoncoins00@gmail.com>
2017-10-21 22:25:44 -0500

    add liqui exchange!

---
+++ app/liqui_client.rb
@@ -0,0 +1,33 @@
+
+require 'open-uri'
+require 'json'
+require './app/exchange'
+
+class LiquiClient
+  attr_accessor :url, :exchange, :trade_pairs_url
+  def initialize()
+    @exchange = Exchange.new('liqui')
+    @url = "https://api.liqui.io/api/3/ticker/"
+    @trade_pairs_url = "https://api.liqui.io/api/3/info"
+  end
+
+  def get_exchange()
+    # first we have to get all the trade-pairs listed on liqui and put them in the url
+    trade_pairs_json = JSON.parse(open(@trade_pairs_url).read)
+    trade_pairs = trade_pairs_json['pairs'].keys()
+    @url += trade_pairs.join('-')
+    source = open(url).read
+    parse_snapshot(JSON.parse(source))
+  end
+
+
+  def parse_snapshot(snapshot)
+    snapshot.each do |key, value| 
+      name = key.gsub('_', '').upcase
+      crypto = Crypto.new(name: name, bid: value["buy"], ask: value["sell"], volume_24h: value["vol"])
+      @exchange.add_crypto(crypto)
+    end
+    @exchange
+  end
+
+end
---
+++ spec/liqui_client_spec.rb
@@ -0,0 +1,24 @@
+require './app/liqui_client'
+require 'uri'
+
+
+RSpec.describe LiquiClient do
+  context "#get_exchange" do
+    it "returns the exchange data" do
+      liqui_client = LiquiClient.new
+      fixture = open("./spec/fixtures/liqui_api_call.json").read;
+      stub_request(:any, Regexp.new(liqui_client.url)).to_return(body: fixture)
+      trade_pair_fixture = open("./spec/fixtures/liqui_pair_info.json").read
+      stub_request(:any, liqui_client.trade_pairs_url).to_return(body: trade_pair_fixture)
+
+     
+      
+      exchange = liqui_client.get_exchange()
+      expect(exchange.name).to eq('liqui')
+      expected_crypto = Crypto.new(name: 'LTCBTC', bid: 0.0097081, ask: 0.00973799, volume_24h: 49.682074394008394)
+      %i[name bid ask volume_24h].each do |value|
+        expect(exchange.cryptos[0].send(value)).to eq(expected_crypto.send(value))
+      end
+    end
+  end
+end
