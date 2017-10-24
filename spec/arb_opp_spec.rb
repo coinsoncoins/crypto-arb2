c1f76276f5ddaa4b4bf49609fdfac354fd47c980
Coins On Coins <coinsoncoins00@gmail.com>
2017-10-24 15:27:35 -0500

    arb_opp#calc_potential_profit test

--- spec/arb_opp_spec.rb
+++ spec/arb_opp_spec.rb
@@ -1,15 +1,23 @@
 
 require './app/arb_opp'
+require './app/bittrex_client'
 
 
 RSpec.describe ArbOpp do
-  context "#get_exchange" do
+  context "#calc_potential_profit" do
     it do
-      crypto_pair1 = CryptoPair.new(name: 'BNT-BTC')
-      crypto_pair2 = CryptoPair.new(name: 'BNT-BTC')
+      bittrex_client = BittrexClient.new
+      hitbtc_client = HitBtcClient.new
+      fixture = open("./spec/fixtures/bittrex_order_book_bnt.json").read;
+      stub_request(:any, Regexp.new(bittrex_client.order_book_url.split('/')[2])).to_return(body: fixture)
+      fixture = open("./spec/fixtures/hitbtc_order_book_bnt.json").read;
+      stub_request(:any, Regexp.new(hitbtc_client.order_book_url.split('/')[2])).to_return(body: fixture)
+
+      crypto_pair1 = CryptoPair.new(name: 'BNT-BTC', exchange: bittrex_client.exchange)
+      crypto_pair2 = CryptoPair.new(name: 'BNT-BTC', exchange: hitbtc_client.exchange)
 
       arb_opp = ArbOpp.new(crypto_pair1, crypto_pair2)
-      #arb_opp.calc_potential_profit
+      expect(arb_opp.calc_potential_profit).to eq(0.00002464)
     end
   end
 end
