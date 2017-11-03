f479228bc722e39f315c3c3b2c35b38e6d382871
Coins On Coins <coinsoncoins00@gmail.com>
2017-11-03 17:20:50 -0500

    add a script that runs app on a loop

--- app/app_loop.rb
+++
@@ -1,8 +0,0 @@
-
-
-while true
-  p "running program"
-  exec("ruby ./app/app.rb")
-  p "sleep"
-  sleep(1)
-end
---
+++ app_loop.sh
@@ -0,0 +1,7 @@
+
+while true; do
+  echo -n "running program";
+  ruby ./app/app.rb
+  sleep 5
+done
+
